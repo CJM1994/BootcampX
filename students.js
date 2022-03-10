ebc22bc1edf7a9a3f7a1860e4003c154f6634657
CJM1994 <connorjohnmullin@gmail.com>
2022-03-10 05:39:56 +0000

    sanitized query

--- students.js
+++ students.js
@@ -8,15 +8,18 @@ const configuration = {
 const pool = new Pool(configuration);
 
 const cohort = process.argv[2];
-const maxResults = process.argv[3];
+const maxResults = process.argv[3] || 5;
+const values = [`%${cohort}%`, maxResults];
 
-pool.query(
+const query =
   `SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
   FROM students JOIN cohorts ON students.cohort_id = cohorts.id
-  WHERE cohorts.name LIKE '%${cohort}%'
-  LIMIT ${maxResults};`,
-)
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;`;
+
+pool.query(query, values)
   .then(res => {
+    console.log(res.rows);
     res.rows.forEach(user => {
       console.log(`${user.name} is in the ${user.cohort} cohort and has a student id of ${user.student_id}`)
     })
